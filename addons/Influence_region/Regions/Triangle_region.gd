a5215518d83fc32eb508942287ec94ee083fd863
Bob mervell <B0bm3rv3ll@gmail.com>
2025-05-03 19:51:28 +0200

    fix: correct triangle start_offset was inverted

--- addons/Influence_region/Regions/Triangle_region.gd
+++ addons/Influence_region/Regions/Triangle_region.gd
@@ -5,31 +5,39 @@ class_name TriangleRegion
 @export_range(.01,100,.01,"or_greater") var length_A_B:float =2:
 	set(new_value):
 		length_A_B = new_value
+		size = (length_A_B +length_B_C +length_C_A)/3
 		on_parameter_updated.emit()
 @export_range(.01,100,.01,"or_greater") var length_B_C:float =2:
 	set(new_value):
 		length_B_C = new_value
+		size = (length_A_B +length_B_C +length_C_A)/3
 		on_parameter_updated.emit()
 @export_range(.01,100,.01,"or_greater") var length_C_A:float =2:
 	set(new_value):
 		length_C_A = new_value
+		size = (length_A_B +length_B_C +length_C_A)/3
 		on_parameter_updated.emit()
 
+var size:float = (length_A_B +length_B_C +length_C_A)/3
+
 ## Returns the list of points needed to draw a triangle
-func _get_triangle_array(center:Vector3,x:float,start_offset:Vector2,triangle_nbr:int) -> Array[Vector3]:
+func _get_triangle_array(center:Vector3,triangle_size:float,start_offset:Vector2,triangle_nbr:int) -> Array[Vector3]:
 	mesh_extremums.append({
 		"max_x": -INF, "min_x": +INF,
 		"max_z": -INF, "min_z": +INF,
 	})
 	var mesh_array:Array[Vector3] = [center] #A
-	mesh_array.append(center + Vector3((length_A_B*x),0,0)) #B	
-	var alpha = acos( ((length_A_B*x)**2 + (length_C_A*x)**2 - (length_B_C*x)**2)/(2*(length_A_B*x)*(length_C_A*x)) )
-	mesh_array.append(center + Vector3(cos(alpha) * (length_C_A*x) ,0,sin(alpha) * (length_C_A*x))) #C
+	mesh_array.append(center + Vector3((length_A_B*triangle_size),0,0)) #B	
+	var alpha = acos( ((length_A_B*triangle_size)**2 + (length_C_A*triangle_size)**2 - (length_B_C*triangle_size)**2)/(2*(length_A_B*triangle_size)*(length_C_A*triangle_size)) )
+	mesh_array.append(center + Vector3(cos(alpha) * (length_C_A*triangle_size) ,0,sin(alpha) * (length_C_A*triangle_size))) #C
 	mesh_array.append(center) #A
-
+	
+	#start_offset = start_offset * radius * (1 - circle_radius/radius)
+	#var start_pos:Vector3 = center + Vector3(start_offset.x,0,start_offset.y)
+	
 	# offset triangle to put centroid in the center
 	var centroid:Vector3 = (mesh_array[0] + mesh_array[1] + mesh_array[2])/3
-	start_offset = start_offset * x * (length_A_B +length_B_C +length_C_A)/3
+	start_offset = start_offset * size * (1- triangle_size/size) 
 
 	var diff = (center+Vector3(start_offset.x,0,start_offset.y)) -centroid
 	for i in range(mesh_array.size()):
@@ -63,9 +71,9 @@ func get_meshs(center:Vector3, nbr_regions:int,start_offset:Vector2) -> Array[Me
 			(length_C_A >= length_A_B +length_B_C) ):
 		return meshs
 	for i in range(1,nbr_regions):
-		var triangle_size:float = i/float(nbr_regions)
+		var triangle_size:float = i/float(nbr_regions) * size
 		var triangle_mesh:MeshInstance3D = draw_multi_line(_get_triangle_array(center,triangle_size,start_offset,i-1))
 		meshs.append(triangle_mesh)
-	var triangle_mesh:MeshInstance3D = draw_multi_line(_get_triangle_array(center,1,start_offset,nbr_regions-1))
+	var triangle_mesh:MeshInstance3D = draw_multi_line(_get_triangle_array(center,size,start_offset,nbr_regions-1))
 	meshs.append(triangle_mesh)
 	return meshs
