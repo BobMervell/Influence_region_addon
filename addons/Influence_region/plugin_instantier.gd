42c8302657ca5ab8565b45c9bb3571ccdd25e9ac
Bob mervell <B0bm3rv3ll@gmail.com>
2025-05-05 11:44:58 +0200

    refacto: removed code smells

--- addons/Influence_region/plugin_instantier.gd
+++ addons/Influence_region/plugin_instantier.gd
@@ -2,10 +2,10 @@
 extends EditorPlugin
 
 
-func _enter_tree():
+func _enter_tree() -> void:
 	add_custom_type("InfluenceRegion","Node",preload("res://addons/Influence_region/Influence_region.gd"),
 		preload("res://addons/Influence_region/Region_icon.png"))
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_custom_type("InfluenceRegion")
